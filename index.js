308b3c4552d7e83884dd86cc85a59464370ae258
Irina Bright <missirinabright@gmail.com>
2019-04-14 04:01:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,3 +33,11 @@ function makeInt(n) {
 function makeInt(n) {
   return parseInt(n, 10);
 }
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
